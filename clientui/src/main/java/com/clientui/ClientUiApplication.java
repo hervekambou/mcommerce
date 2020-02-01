594f9550b5849860322a6bbbb49b5324f4b1224e
hervekambou <khadev@gmx.com>
2020-02-02 00:33:21 +0100

    Ajout de swagger aux micro services

--- clientui/src/main/java/com/clientui/ClientUiApplication.java
+++ clientui/src/main/java/com/clientui/ClientUiApplication.java
@@ -3,9 +3,11 @@ package com.clientui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients("com.clientui")
+@EnableSwagger2
 public class ClientUiApplication {
 
 	public static void main(String[] args) {
